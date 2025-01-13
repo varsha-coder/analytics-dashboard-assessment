b1eb15c1294637fefd7d189c625319cd7958b747
NAGAVARSHINI R K <nvarshinirk@gmail.com>
2025-01-13 21:38:55 +0530

    Upgraded React to version 19 and fixed dependencies

--- frontend/src/utils/sidebar.tsx
+++ frontend/src/utils/sidebar.tsx
@@ -87,7 +87,7 @@ const Sidebar = () => {
         year: prevFilters.year || mainContext.yearList[0] || "",
       }));
     }
-
+    // Empty dependency array to run only on mount
   }, []);
 
   useEffect(() => {
